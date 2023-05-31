a3b1544dc3d1d08d0871cf64c2daebd70fef8695
PabloSan1997 <jos_pab_997@hotmali.com>
2023-05-30 18:07:48 -0600

    Se corrigio bug de la parte de carga

--- client/src/contexto/index.tsx
+++ client/src/contexto/index.tsx
@@ -33,6 +33,7 @@ export function Provedor({ children }: Children) {
             })();
         }else{
             setTextos({...textos, des:""})
+            setLoading(false);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [activar]);
