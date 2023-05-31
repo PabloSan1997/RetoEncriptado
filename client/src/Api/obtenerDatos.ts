7bc9fd42f17faf9d653ea25f44b3655876288fb6
PabloSan1997 <jos_pab_997@hotmali.com>
2023-05-30 18:00:27 -0600

    Se alisto la parte del cliente para produccion

--- client/src/Api/obtenerDatos.ts
+++ client/src/Api/obtenerDatos.ts
@@ -1,7 +1,7 @@
 
 
 export async function  procesar(accion:string, solicitud:Solicitud):Promise<Info> {
-        const data = await fetch(`http://localhost:3001/api/v1/proceso/${accion}`, solicitud);
+        const data = await fetch(`https://encriptador.onrender.com/api/v1/proceso/${accion}`, solicitud);
         const info = await data.json();
         if(!data.ok){
             throw `${info.error} ${info.message}`;
